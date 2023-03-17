4635f1c0171341f6dd0e9e07a65ea3e454984f68
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-17 09:57:39 +0530

    Create subsequences.cpp

---
+++ subsequences.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+
+void solve(string str, string output, int index, vector<string>& ans){
+	// Base case
+	if(index >= str.length()){
+		if(output.length() > 0)
+		 ans.push_back(output);
+	    return;
+	}
+
+	// Exclude
+	solve(str, output, index+1, ans);
+
+	// Include
+	char element = str[index];
+	output.push_back(element);
+	solve(str, output, index+1 , ans);
+
+	 
+}
+vector<string> subsequences(string str){
+	vector<string> ans;
+	string output = "";
+  int index = 0;
+	solve(str, output, index, ans);
+	return ans;
+	
+}
