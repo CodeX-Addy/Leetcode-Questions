3e83ee18c0e2e9514e706025b99b0c67f2e3f709
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2024-01-08 00:59:07 +0530

    inorderTraversal.cpp

--- inorderTraversal.cpp
+++ inorderTraversal.cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// INORDER TRAVERSAL USING MORRIS TRAVERSAL FOR SPACE OPTIMIZATION
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
