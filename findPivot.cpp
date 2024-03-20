5a9bba82bdc2ad6d32aa2f98d21a7da499ff578a
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-20 22:51:22 +0530

    .

--- findPivot.cpp
+++ findPivot.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    
+    //Brute force
+    class Solution {
+        public:
+         int bruteForce(vector<int>& nums){
+                for(int i=0; i<nums.size(); ++i){
+                        int lsum = 0;
+                        int rsum = 0;
+                        for(int j=0; j<i; ++j){
+                            lsum += nums[j];
+                        }
+                        for(int j=i+1; j<nums.size(); ++j){
+                            rsum += nums[j];
+                        }
+                        if(lsum == rsum)
+                            return i;
+                    }
+                
+                return -1;
+            }
+            int pivotIndex(vector<int>& nums) {
+            int ans= bruteForce(nums);
+            return ans;
+        }
+};
     int pivotIndex(vector<int>& nums) {
         // Initialize rightSum to store the sum of all the numbers strictly to the index's right
         int rightSum = accumulate(nums.begin(), nums.end(), 0);
