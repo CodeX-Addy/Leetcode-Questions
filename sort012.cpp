1a9a755944d356a5a30a1800371123f60713ff38
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-21 22:07:19 +0530

    .

--- sort012.cpp
+++ sort012.cpp
@@ -1,4 +1,44 @@
 #include <bits/stdc++.h> 
+
+//Approach1 : Brute force
+//sort(a.begin(, a.end())) --> TC- O(nlogn) SC - O(1)
+
+
+//Approach2: By counting
+void sortColors(vector<int>& nums) {
+        vector<int> ans;
+        //SORT --> O(nlogn)
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == 0)
+                ++cnt0;
+        }
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == 1)
+                ++cnt1;
+        }
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == 2)
+                ++cnt2;
+        }
+
+        for(int i=0; i<cnt0; ++i){
+            nums[i] = 0;
+        }
+
+        for(int i=cnt0; i<cnt0+cnt1; ++i){
+            nums[i] = 1;
+        }
+
+        for(int j=cnt0+cnt1; j<cnt0+cnt1+cnt2; ++j){
+            nums[j] = 2;
+        }
+    }
+
+
 void sort012(int *arr, int n)
 {
    int s=0,e=n-1,mid=0;
