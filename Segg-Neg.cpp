56263f696587167d407978727a176aa9f9ad842e
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-03-22 13:11:25 +0530

    Seggregate negative numbers on left

--- Segg-Neg.cpp
+++ Segg-Neg.cpp
@@ -3,12 +3,31 @@
 #include<vector>
 using namespace std;
 
+void seggregate(vector<int>&v){
+    int s = 0;
+    int e = v.size()-1;
+    while(s<e){
+        if(v[s] < 0)
+            ++s;
+        else if(v[e] >= 0)
+            --e;
+        else{
+            swap(v[s],v[e]);
+            ++s;
+            --e;
+        }
+    }
+}
+
 
 int main(){
     vector<int> v = {4,-1,2,-3,5,6};
+    
+    //sort(v.begin(), v.end());
+    seggregate(v);
 
-    sort(v.begin(), v.end());
     for(int i : v)
         cout << i << " ";
+
 
 }
