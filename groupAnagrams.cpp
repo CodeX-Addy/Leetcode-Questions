9fd736aebb036e18c709e0bf5a354affef8d4333
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-04-11 16:52:11 +0530

    LeetCode_49

--- groupAnagrams.cpp
+++ groupAnagrams.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-
+    //Hash function 
     std::array<int, 256> hash(string s){
         std::array<int, 256> hash = {0};
         for(int i=0; i<s.size(); ++i){
@@ -13,7 +13,7 @@ public:
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<std::array<int, 256>, vector<string>>mp;
-
+  
         for(auto str : strs){
             mp[hash(str)].push_back(str);
         }
