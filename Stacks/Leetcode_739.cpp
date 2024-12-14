ef01bbf2f88a79ea28cd989da55208863ca48615
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2024-12-14 18:47:32 +0530

    daily temperatures

---
+++ Stacks/Leetcode_739.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>ans(temperatures.size(), 0);
+        stack<int>st;
+
+        for(int i=0; i<temperatures.size(); ++i){
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
