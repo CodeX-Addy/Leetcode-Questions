0cceb77b6f141b3ab75fcd56cfc60b54ec33177a
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2024-12-14 16:52:05 +0530

    number of laser beams

---
+++ Arrays & Strings/LeetCode_2125.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int countDevice(string&binary){
+        int cnt = 0;
+        for(auto i:binary){
+            cnt += i - '0';
+        }
+        return cnt;
+    }
+
+    int numberOfBeams(vector<string>& bank) {
+        vector<int>devices;
+
+        for(auto row:bank)
+            devices.push_back(countDevice(row));
+
+        int beams = 0;
+        for(int i=0; i<devices.size(); ++i){
+            int j = i+1;
+            while(j < devices.size()){
+                beams += devices[i]*devices[j];
+                if(devices[j] == 0)
+                    j++;
+                else
+                    break;
+            }
+        }
+        return beams;
+    }
+};
